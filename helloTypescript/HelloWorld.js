f454c8cfbd25b8b51a307ea81595521876e71a9d
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-28 11:43:21 +0200

    made hello Typescript

---
+++ helloTypescript/HelloWorld.js
@@ -0,0 +1,7 @@
+var message = 'Hello, Typescript!';
+console.log(message);
+//create a new heading1 element
+var heading = document.createElement('h1');
+heading.textContent = message;
+//add the heading to the document
+document.body.appendChild(heading);
---
+++ helloTypescript/HelloWorld.ts
@@ -0,0 +1,7 @@
+let message: string = 'Hello, Typescript!';
+console.log(message);
+//create a new heading1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+//add the heading to the document
+document.body.appendChild(heading);
