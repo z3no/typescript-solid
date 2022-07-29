af3e853f5624fb79f49c6cc327ea3ff3800bb363
Zeno Driesen <zeno.driesen@gmail.com>
2022-07-29 16:59:36 +0200

    compiled ts to js

--- helloTypescript/HelloWorld.js
+++ helloTypescript/HelloWorld.js
@@ -1,7 +1,8 @@
-var message = 'Hello, Typescript!';
+"use strict";
+let message = 'Hello, Typescript!';
 console.log(message);
 //create a new heading1 element
-var heading = document.createElement('h1');
+let heading = document.createElement('h1');
 heading.textContent = message;
 //add the heading to the document
 document.body.appendChild(heading);
