0f5737df9356788e436e5b88880d8dc4b480d37e
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-01 14:57:08 +0200

    several steps importing the engine, musicplayer and fueltank into the car. Created the properties radio, engine and fueltank. Adjusted the drive method, created a constructor where we instantiate the radio, fueltank and engine. Also getting the radio and engine.

--- 0.S/model/car.ts
+++ 0.S/model/car.ts
@@ -1,26 +1,35 @@
 import { Engine } from "./engine";
+import { MusicPlayer } from "./musicPlayer";
+import { FuelTank } from "./fuelTank";
 
 export class Car {
     //it is convention to start property names in TypeScript with an underscore.
     // If you want to known why, remove the underscore and see if your compiler is throwing you an error!
     private _miles : number = 0;
+    private _radio : MusicPlayer;
     private _engine : Engine;
-
+    private _fuelTank: FuelTank;
     //By changing this variable to readonly I have in essence created a property constant.
     // the only subtle difference is that you can write once to the variable inside the constructor
-
-    private readonly FUEL_MILEAGE: number = 10;
+    private FUEL_MILEAGE: number = 10;
 
     constructor() {
-        this._engine = new Engine(100);
+        this._engine = new Engine();
+        this._radio = new MusicPlayer();
+        this._fuelTank = new FuelTank(100)
     }
 
     get miles(): number {
         return this._miles;
     }
 
-//Take attention to these getters and setters
+    get radio(): MusicPlayer {
+        return this._radio;
+    }
 
+    get engine(): Engine {
+        return this._engine;
+    }
 
     //When a value can only go one way (you add fuel, consuming fuel is handled by the car itself)
     // it is better to provide a specific method for this instead of a generic setter.
@@ -28,14 +37,14 @@ export class Car {
 
 
     drive() {
-        if(this._engine.engineStatus === false || this._engine.fuel <= 0) {
+        if(this._engine.engineStatus === false || this._fuelTank.fuel <= 0) {
             //what I am doing here is a good principle called "failing early"
             // If you have some conditions you need to check, that will exclude most of the code in your function check that first
             // This prevents your "happy path" of code to be deeply indented.
             return;
         }
 
-        this._engine.consumeFuel();
+        this._fuelTank.consumeFuel();
         this._miles += this.FUEL_MILEAGE;
     }
 }
