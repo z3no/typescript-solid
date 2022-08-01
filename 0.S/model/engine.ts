da91303150c2cc39d6659bbba50c7079eaa95beb
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-01 14:51:53 +0200

    removed fueltank removed the fueltank because it is NOT inside an engine it is linked, it is inside a car

--- 0.S/model/engine.ts
+++ 0.S/model/engine.ts
@@ -1,12 +1,5 @@
-import { FuelTank } from "./fuelTank";
-
 export class Engine {
     private _engineStatus: boolean = false;
-    private _fuelTank: FuelTank;
-
-    constructor(capacity: number) {
-        this._fuelTank = new FuelTank(capacity)
-    }
 
     get engineStatus(): boolean {
         return this._engineStatus;
@@ -18,9 +11,5 @@ export class Engine {
 
     turnEngineOff() {
         this._engineStatus = false;
-    }
-
-    get fuelTank(): FuelTank {
-        return this.fuelTank;
     }
 }
