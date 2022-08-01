99606db5fabd70cfd90f56fbe29dc129aad1843d
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-01 16:24:05 +0200

    created a file for the parrot class

---
+++ 1.O/model/parrot.ts
@@ -0,0 +1,15 @@
+class Parrot {
+    private _name;
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get type() {
+        return 'parrot';
+    }
+}
