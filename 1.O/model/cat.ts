22feb34d83234153aae8eb034f80583080981f0c
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-01 16:23:35 +0200

    created a file for the cat class

---
+++ 1.O/model/cat.ts
@@ -0,0 +1,15 @@
+class Cat {
+    private _name;
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get type() {
+        return 'cat';
+    }
+}
