04b323ceca974521125e7ca1b5fd7b90cee4639e
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-01 14:09:52 +0200

    added fuel parts so I added everything that I think is related to the fuel into the fueltank class

---
+++ 0.S/model/fuelTank.ts
@@ -0,0 +1,16 @@
+class FuelTank {
+    private _fuel : number = 0;
+    private readonly MAXIMUM_FUEL_CAPACITY: number;
+
+    constructor(MAXIMUM_FUEL_CAPACITY: number) {
+        this.MAXIMUM_FUEL_CAPACITY = MAXIMUM_FUEL_CAPACITY;
+    }
+
+    addFuel(fuel : number) {
+        this._fuel = Math.min(fuel + this._fuel, this.MAXIMUM_FUEL_CAPACITY);
+    }
+
+    consumeFuel() {
+        this._fuel -= 1;
+    }
+}
