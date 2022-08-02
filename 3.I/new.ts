63f43b11c7a7e4ad5a145ea1883290dcb811312e
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-02 16:10:14 +0200

    created a googleBot class

--- 3.I/new.ts
+++ 3.I/new.ts
@@ -62,3 +62,68 @@ class Admin implements PasswordAuth {
     }
 
 }
+
+// Feature request
+class GoogleBot implements GoogleAuth {
+
+    private _googleToken : string;
+
+    checkGoogleLogin(token: string): boolean {
+        // return "this will not work";
+        return (token === this._googleToken);
+    }
+
+    setGoogleToken(token: string) {
+        this._googleToken = token;
+    }
+}
+
+const passwordElement = <HTMLInputElement>document.querySelector('#password');
+const typePasswordElement = <HTMLInputElement>document.querySelector('#typePassword');
+const typeGoogleElement = <HTMLInputElement>document.querySelector('#typeGoogle');
+const typeFacebookElement = <HTMLInputElement>document.querySelector('#typeFacebook');
+const loginAsAdminElement = <HTMLInputElement>document.querySelector('#loginAsAdmin');
+const resetPasswordElement = <HTMLAnchorElement>document.querySelector('#resetPassword');
+
+let guest = new User();
+let admin = new Admin();
+let googleBot = new GoogleBot();
+
+document.querySelector('#login-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let user = loginAsAdminElement.checked ? admin : guest;
+
+    if(!loginAsAdminElement.checked) {
+        user.setGoogleToken('secret_token_google');
+        user.setFacebookToken('secret_token_fb');
+    }
+    debugger;
+
+    let auth = false;
+    switch(true) {
+        case typePasswordElement.checked:
+            auth = user.checkPassword(passwordElement.value);
+            break;
+        case typeGoogleElement.checked:
+            auth = user.checkGoogleLogin('secret_token_google');
+            break;
+        case typeFacebookElement.checked:
+            debugger;
+            auth = user.getFacebookLogin('secret_token_fb');
+            break;
+    }
+
+    if(auth) {
+        alert('login success');
+    } else {
+        alert('login failed');
+    }
+});
+
+resetPasswordElement.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    let user = loginAsAdminElement.checked ? admin : guest;
+    user.resetPassword();
+});
