ab2074df68d8f707eb8e252582bdbc63b6c15e0b
Zeno Driesen <zeno.driesen@gmail.com>
2022-08-03 11:51:26 +0200

    created a gasoven class implements the oveninterface

---
+++ 4.D/Models/GasOven.ts
@@ -0,0 +1,37 @@
+import {OvenInterface} from "../Interfaces/OvenInterface";
+
+class GasOven implements OvenInterface {
+    private _isOn;
+
+    public on(): void {
+        setTimeout(function (){
+            document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : THE GAS IS ON!</p>";
+        }, 1000);
+        console.log("THE GAS IS ON!"); //insert fart humor here
+        this._isOn = true;
+    }
+
+    public off(): void {
+        setTimeout(function (){
+            document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : THE GAS IS OFF!</p><hr>";
+        }, 3000);
+        console.log("THE GAS IS OFF!"); //insert fart humor here
+        this._isOn = false;
+    }
+
+    public bake(item: string): void {
+        if(this._isOn) {
+            setTimeout(function (){
+                document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : Now baking " + item + " !</p>";
+            }, 2000);
+            console.log("Now baking " + item + "!");
+        }
+        else
+        {
+            setTimeout(function (){
+                document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : there is no gas!</p>";
+            }, 2000);
+            console.log("there is no gas!");//insert fart humor here
+        }
+    }
+}
